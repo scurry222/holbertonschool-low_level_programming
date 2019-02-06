65cd75cf6219bc2b17a739978693055faa149a9d
scurry222 <scurry222@gmail.com>
2019-02-06 19:24:30 +0000

    locally tested content

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+* main - use puts to print a string
+*
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - use printf to print a string
+*
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = ("with proper grammar, but the outcome is a piece of art,");
+
+	printf("%s\n", str);
+
+	return (0);
+}

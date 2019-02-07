19456f920caecdc8c4dc97b5870b3359a96b15c1
scurry222 <scurry222@gmail.com>
2019-02-07 22:24:21 +0000

    all possible combos of two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print 00 through 99, separated by commas and spaces
+*
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+	int c;
+	int c2;
+	int c3;
+	int c4;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (c2 = 48; c2 <= 56; c2++)
+		{
+			for (c3 = 48; c3 <= 57; c3++)
+			{
+				for (c4 = 49; c4 <= 57; c4++)
+				{
+					if (c != c2 || c2 != c3 || c3 != c4 || c != c4 || c != c3)
+					{
+					putchar(c);
+					putchar(c2);
+					putchar(' ');
+					putchar(c3);
+					putchar(c4);
+					if (c != 57 || c2 != 56 || c3 != 57 || c4 != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}

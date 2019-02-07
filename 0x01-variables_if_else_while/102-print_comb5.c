0b60f32fdf0f04ad61549d54906225248c0fdd87
scurry222 <scurry222@gmail.com>
2019-02-07 23:54:26 +0000

    deals with 2 numbers together, dealt with digits in printing process

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,33 +10,20 @@
 int main(void)
 {
 	int c;
-	int c2;
-	int c3;
-	int c4;
+	int d;
 
-	for (c = 48; c <= 57; c++)
+	for (c = 0; c <= 98; c++)
 	{
-		for (c2 = 48; c2 <= 56; c2++)
+		for (d = c + 1; d <= 99; d++)
 		{
-			for (c3 = c; c3 <= 57; c3++)
-			{
-				for (c4 = c2 + 1; c4 <= 57; c4++)
-				{
-					if (c != c2 || c2 != c3 || c3 != c4 || c2 != c4 || c != c3 || c != c4)
-					{
-					putchar(c);
-					putchar(c2);
-					putchar(' ');
-					putchar(c3);
-					putchar(c4);
-					if (c != 57 || c2 != 56 || c3 != 57 || c4 != 57)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
-				}
-			}
+			putchar ((c / 10) + '0');
+			putchar ((c % 10) + '0');
+			putchar (' ');
+			putchar ((d / 10) + '0');
+			putchar ((d % 10) + '0');
+			if (c != 98 || d != 99)
+				putchar(',');
+				putchar(' ');
 		}
 	}
 	putchar ('\n');

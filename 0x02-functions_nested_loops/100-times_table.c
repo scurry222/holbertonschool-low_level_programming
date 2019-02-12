a3006674e4a43181d3e5433235c3adf3ec8c45e6
scurry222 <scurry222@gmail.com>
2019-02-12 21:13:28 +0000

    extra arithmetic on ten's digit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,43 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
+* times_table - print a times table with only putchar
+* @n: passed in number to act as table size
 *
-*
-*
+* Return: void
 */
 
 void print_times_table(int n)
 {
 	int x;
 	int y;
-	int sum;
+	int prod;
 
-	if (n > 0 && n < 15)
+	if (n <= 15 && n >= 0)
 	{
 		for (y = 0; y <= n; y++)
 		{
-			for (x = 0; x <= n; x++)
+			_putchar ('0');
+			for (x = 1; x <= n; x++)
 			{
-				sum = x * y;
-				if (x != n)
+				prod = x * y;
+				if (x <= n)
 				{
-					if (sum < 10)
-						printf("%d,   ", sum);
-					else if (sum < 100)
-						printf("%d,  ", sum);
+					_putchar(',');
+					_putchar(' ');
+					if (prod > 99)		
+						_putchar(prod / 100 + '0');
 					else
-						printf("%d, ", sum);
+						_putchar(' ');
+					if (prod > 9)
+						_putchar((prod / 10)  % 10 + '0');
+					else
+						_putchar(' ');
+					_putchar(prod % 10 + '0');
 				}
-				else
-					printf ("%d\n", sum);
+				if (x == n)
+				_putchar('\n');
 			}
-			printf ("\n");
 		}
 	}
 }

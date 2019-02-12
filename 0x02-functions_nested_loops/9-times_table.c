c81b72852b26a8993ba5e41c380718a16b91c4f6
scurry222 <scurry222@gmail.com>
2019-02-12 19:37:31 +0000

    fixed var names to be accurate, code to not print extra dot on first double digit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,34 +10,35 @@ void times_table(void)
 {
 	int x;
 	int y;
-	int sum;
+	int prod;
 
-	for (x = 0; x <= 9; x++)
+	for (y = 0; y <= 9; y++)
 	{
-		for (y = 0; y <= 9; y++)
+		_putchar('0');
+		for (x = 1; x <= 9; x++)
 		{
-			sum = x * y;
-			if (sum <= 9)
+			prod = x * y;
+			if (prod <= 9)
 			{
-				_putchar(sum + '0');
-				if (y != 9)
+				if (x != 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(prod + '0');
 				}
 			}
 			else
 			{
-				_putchar(sum / 10 + '0');
-				_putchar(sum % 10 + '0');
-				if (y != 9)
+				if (x != 9)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(prod / 10 + '0');
+					_putchar(prod % 10 + '0');
 				}
 			}
-			if (y == 9)
+			if (x == 9)
 			_putchar('\n');
 		}
 	}

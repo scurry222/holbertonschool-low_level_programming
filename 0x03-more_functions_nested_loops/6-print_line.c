65e0092ee15ea266606bdf747efa5038767a6b25
scurry222 <scurry222@gmail.com>
2019-02-14 21:53:51 +0000

    prints 0 for n correctly

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -11,7 +11,12 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
-		_putchar('_');
-	_putchar('\n');
+	if (n == 0)
+		_putchar ('\n');
+	else
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
 }

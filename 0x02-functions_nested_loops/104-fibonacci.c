68ba9c5b96cdc5a8056d039e542be50516ef47c8
scurry222 <scurry222@gmail.com>
2019-02-14 00:53:41 +0000

    fixed dup number, with different arithmetic

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,6 +17,8 @@ int main(void)
 	unsigned long twohalf;
 	unsigned long twohalf2;
 	unsigned long split = 1000000000;
+	unsigned long sum1;
+	unsigned long sum2;
 	int i;
 
 	printf("%lu, %lu", older, old);
@@ -24,22 +26,23 @@ int main(void)
 	for (i = 1; i < 91; i++)
 	{
 		curr = old + older;
+		printf(", %lu", curr);
 		older = old;
 		old = curr;
-		printf(", %lu", old);
 	}
-	onehalf = (old / split);
-	onehalf2 = (old % split);
-	twohalf = (curr / split);
-	twohalf2 = (curr % split);
-	for (i = 92; i <= 98; ++i)
+	onehalf = (older / split);
+	twohalf = (old / split);
+	onehalf2 = (older % split);
+	twohalf2 = (old % split);
+	for (i = 92; i <= 97; ++i)
 	{
-		onehalf = twohalf - onehalf;
-		onehalf2 = twohalf2 - onehalf2;
-		twohalf += onehalf;
-		twohalf2 += onehalf2;
-
-		printf(", %lu%lu", (onehalf2 / split) + onehalf, onehalf2 % split);
+		sum2 = (onehalf2 + twohalf2) % split;
+		sum1 = (onehalf + twohalf + (sum2 / split));
+		onehalf = twohalf;
+		onehalf2 = twohalf2;
+		twohalf = sum1;
+		twohalf2 = sum2;
+		printf(", %lu%lu", sum1, sum2);
 	}
 	putchar('\n');
 	return (0);

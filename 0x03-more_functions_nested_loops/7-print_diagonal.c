0cf5d0c668af30665c08d07c077f281a27c765b2
scurry222 <scurry222@gmail.com>
2019-02-14 22:01:12 +0000

    o and negative edge case

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -12,20 +12,25 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i <= n; i++)
+	if (n <= 0)
+		_putchar ('\n');
+	else
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i < n; i++)
 		{
-			if (n <= 0)
-				_putchar('\n');
-			else if (i == j)
+			for (j = 0; j < n; j++)
 			{
-				_putchar ('\\');
-				_putchar ('\n');
-				break;
+				if (n <= 0)
+					_putchar('\n');
+				else if (i == j)
+				{
+					_putchar ('\\');
+					_putchar ('\n');
+					break;
+				}
+				else
+					_putchar (' ');
 			}
-			else
-				_putchar (' ');
 		}
 	}
 }

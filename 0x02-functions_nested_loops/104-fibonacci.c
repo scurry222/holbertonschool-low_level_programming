95967a24bea6f272ceed3a04901969f1f38f48ee
scurry222 <scurry222@gmail.com>
2019-02-14 00:25:59 +0000

    fixed overflowing

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,37 +9,38 @@
 
 int main(void)
 {
-	unsigned long curr;
 	unsigned long old = 2;
 	unsigned long older = 1;
+	unsigned long curr;
 	unsigned long onehalf;
+	unsigned long onehalf2;
 	unsigned long twohalf;
+	unsigned long twohalf2;
+	unsigned long split = 1000000000;
 	int i;
 
-	printf("%lu, %lu, ", older, old);
+	printf("%lu, %lu", older, old);
 
-	for (i = 0; i <= 96; i++)
+	for (i = 1; i < 91; i++)
 	{
 		curr = old + older;
-
-		if (i < 89)
-		{
-			printf("%lu", curr);
-		}
-		else
-		{
-			onehalf = ((old / 10000000) + (older / 10000000));
-			printf("%lu", onehalf);
-			twohalf = ((old % 10000000) + (older % 10000000));
-			printf("%lu", twohalf);
-		}
-
-		if (i == 96)
-			putchar('\n');
-		else
-			printf(", ");
+		printf(", %lu", old);
 		older = old;
 		old = curr;
 	}
+	onehalf = (old / split);
+	onehalf2 = (old % split);
+	twohalf = (curr / split);
+	twohalf2 = (curr % split);
+	for (i = 92; i <= 98; ++i)
+	{
+		onehalf = twohalf - onehalf;
+		onehalf2 = twohalf2 - onehalf2;
+		twohalf += onehalf;
+		twohalf2 += onehalf2;
+
+		printf(", %lu%lu", (onehalf2 / split) + onehalf, onehalf2 % split);
+	}
+	putchar('\n');
 	return (0);
 }

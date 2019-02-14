862189b8f114c97a42a19496d99855fdf7ce0b5f
scurry222 <scurry222@gmail.com>
2019-02-14 22:06:24 +0000

    fixed divisable by 5 and 3

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,12 +14,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("Fizz Buzz");
 		else
 			printf("%d", i);
 		if (i != 100)

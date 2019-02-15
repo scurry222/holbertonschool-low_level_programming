57cb532efdd45efc028fdebf75cae5af9507b8d8
scurry222 <scurry222@gmail.com>
2019-02-15 01:47:16 +0000

    prime factor printer

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -10,34 +10,21 @@
 
 int main(void)
 {
-	unsigned long bigprime = 0;
-	int prime;
-
 	unsigned long i;
-	unsigned long j;
 	unsigned long num = 612852475143;
 
 
-	while (num % 2)
+	while (num % 2 == 0)
 		num = num / 2;
-	for (i = 3; i <= 9999999; i = i + 2)
+	for (i = 3; i <= 999999; i = i + 2)
 	{
-		if (num % i == 0)
+		while (num % i == 0)
 		{
 			num = num / i;
-			prime = 1;
-			for (j = 3; j <= i / 5; j = j + 2)
-				if (i % j == 0)
-				{
-					prime = 0;
-				}
-			if (prime)
-			{
-				bigprime = i;
-			}
 		}
 	}
+
 	if (num > 2)
-		printf("%lu\n", bigprime);
+		printf("%lu\n", num);
 	return (0);
 }

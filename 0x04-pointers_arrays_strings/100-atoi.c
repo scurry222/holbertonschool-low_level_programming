17cce086c8900fbea5105b09a23bd7f2ab05d712
scurry222 <scurry222@gmail.com>
2019-02-19 23:54:20 +0000

    making number negative sooner

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -11,6 +11,7 @@ int _atoi(char *s)
 {
 	int num = 0;
 	int n = 0;
+	int digit;
 	int i = 0;
 	int length;
 	int sign = 0;
@@ -21,17 +22,18 @@ int _atoi(char *s)
 
 	for (i = 0; i < length; i++)
 	{
+		digit = s[i] - '0';
 		if (s[i] == '-')
 			sign++;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			num = 1;
-			n = 10 * n + (s[i] - '0');
+			if (sign % 2 != 0)
+				digit = -digit;
+			n = 10 * n + digit;
 		}
 		else if ((s[i] < '0' || s[i] > '9') && num == 1)
 			break;
 	}
-	if (sign % 2 != 0)
-		n = n * -1;
 	return (n);
 }

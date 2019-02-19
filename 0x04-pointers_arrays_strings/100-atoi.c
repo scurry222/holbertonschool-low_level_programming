0bb64bff67912be60219d59401ef9eb2f90c99e8
scurry222 <scurry222@gmail.com>
2019-02-19 20:08:56 +0000

    atoi with complicated sign cases

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+*
+*
+*
+*
+*/
+
+int _atoi(char *s)
+{
+	int num = 0;
+	int n = 0;
+	int i = 0;
+	int length;
+	int sign = 0;
+
+	for (i = 0; s[i]; i++)
+		;
+	length = i;
+
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] == '-')
+			sign++;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = 1;
+			n = 10 * n + (s[i] - '0');
+		}
+		else if ((s[i] < '0' || s[i] > '9') && num == 1)
+			break;
+	}
+	if (sign % 2 != 0)
+		n = n * -1;
+	return(n);
+}

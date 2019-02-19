d2ec23a193d16df6a23fd0b6d10bf97d34f388bf
scurry222 <scurry222@gmail.com>
2019-02-19 18:36:30 +0000

    printing str[i] instead of i

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -17,9 +17,9 @@ void puts_half(char *str)
 
 	if (length % 2 == 0)
 		for (i = length / 2; i < length; i++)
-			_putchar(i + '0');
+			_putchar(str[i]);
 	else
 		for (i = (length - 1) / 2; i <= length; i++)
-			_putchar(i + '0');
+			_putchar(str[i]);
 	_putchar('\n');
 }

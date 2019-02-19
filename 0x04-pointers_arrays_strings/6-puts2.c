5ebbdfe61403275938e4d55795450a598ae28255
scurry222 <scurry222@gmail.com>
2019-02-19 17:47:17 +0000

    rewrote function to be able to print even numbered strings

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i + 1]; i = i + 2)
-		_putchar(i + '0');
-	for (i = 0; str[i] <= '\0'; i++)
-		if (str[i] == '\0')
-			_putchar(i);
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(i + '0');
+		}
+	}
 	_putchar('\n');
 }

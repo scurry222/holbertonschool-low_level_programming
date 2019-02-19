bd41e58d1bb92836c12a491ce71ef64f71b5ec79
scurry222 <scurry222@gmail.com>
2019-02-19 17:17:52 +0000

    adding null byte

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i + 1] != '\0'; i = i + 2)
+	for (i = 0; str[i + 1]; i = i + 2)
 		_putchar(i + '0');
+	for (i = 0; str[i] <= '\0'; i++)
+		if (str[i] == '\0')
+			_putchar(i);
 	_putchar('\n');
 }

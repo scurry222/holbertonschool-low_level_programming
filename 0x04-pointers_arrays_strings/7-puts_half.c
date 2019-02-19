2e1aeaadfaeb71407f16b527d80e3757fc5a86c1
scurry222 <scurry222@gmail.com>
2019-02-19 18:55:55 +0000

    starting at length -1, with length -2

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -16,10 +16,18 @@ void puts_half(char *str)
 	length = i;
 
 	if (length % 2 == 0)
+	{
 		for (i = length / 2; i < length; i++)
+		{
 			_putchar(str[i]);
+		}
+	}
 	else
-		for (i = (length - 1) / 2; i < length; i++)
+	{
+		for (i = (length - 2) / 2; i < length; i++)
+		{
 			_putchar(str[i]);
+		}
+	}
 	_putchar('\n');
 }

e10294cd81912995fa92509983d851a3d47be800
scurry222 <scurry222@gmail.com>
2019-02-19 18:40:52 +0000

    iterating to less than length

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 		for (i = length / 2; i < length; i++)
 			_putchar(str[i]);
 	else
-		for (i = (length - 1) / 2; i <= length; i++)
+		for (i = (length - 1) / 2; i < length; i++)
 			_putchar(str[i]);
 	_putchar('\n');
 }

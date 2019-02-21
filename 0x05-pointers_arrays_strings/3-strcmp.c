806f17f57d1f0e3c29f7f71bbfc68d344f9a3109
scurry222 <scurry222@gmail.com>
2019-02-21 21:18:00 +0000

    rewriting function to pass checker

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (*s1 && *s2 && s1[i] == s2[i])
+	for (i = 0; s1[i] && s2[i]; i++)
 	{
-		s1++;
-		s2++;
-		i++;
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
 	}
-
-	return (*s1 - *s2);
+	return(0);
 }

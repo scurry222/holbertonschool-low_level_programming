17d25df4c4c0f965e9c4980577669e7b0206f2fd
scurry222 <scurry222@gmail.com>
2019-02-26 00:17:18 +0000

    set value of string to a character

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* set_string - set string to a character
+* @s: double pointer to first string
+* @to: pointer to second string
+*
+* Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

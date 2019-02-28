613813bea12cab8df1493320a1ea2824f3645698
scurry222 <scurry222@gmail.com>
2019-02-28 04:37:56 +0000

    comparing two strings with asterisk usage

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* wildcmp - compare two strings recursively, with asterisk usage
+* @s1: string 1, does not include asterisk
+* @s2: string 2, may include asterick
+*
+* Return: 1 if strings compare, 0 if not
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp((s1 + 1), (s2 + 1)));
+
+	if (*(s2) != '*')
+		return (0);
+
+	if (*(s2 + 1) == '*')
+		return (wildcmp(s1, (s2 + 1)));
+
+	if (*(s1 + 1) == *(s2 + 1))
+		return (wildcmp((s1 + 1), (s2 + 1)));
+
+	return (wildcmp((s1 + 1), s2));
+}

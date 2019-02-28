bb709029fe0547d66a641faf80fb34888df8e6ab
scurry222 <scurry222@gmail.com>
2019-02-28 17:28:10 +0000

    restructuring - works with more cases

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -11,23 +11,18 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
 
-	if (*s1 == *s2)
-		return (wildcmp((s1 + 1), (s2 + 1)));
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
-	if (*s2 != '*')
-		return (0);
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-	if (*(s2 + 1) == '*')
-		return (wildcmp(s1, (s2 + 1)));
-
-	if (*s1 != *(s2 + 1) && *s1 == '\0')
-		return (0);
-
-	if (*(s1 + 1) == *(s2 + 1))
-		return (wildcmp((s1 + 1), (s2 + 1)));
-
-	return (wildcmp((s1 + 1), s2));
+	return (0);
 }

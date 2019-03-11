321d34832876b139f25c232065f3cd7bf26c5d78
scurry222 <scurry222@gmail.com>
2019-03-11 16:26:39 +0000

    fun with preprocessor

---
+++ 0x0C-preprocessor/1-pi.h
@@ -0,0 +1,8 @@
+#ifndef PI_H
+#define PI_H
+
+#define PI 3.14159265359
+
+typedef int make_iso_compilers_happy;
+
+#endif
---
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? (-x) : (x))
+
+typedef int make_iso_compilers_happy;
+
+#endif

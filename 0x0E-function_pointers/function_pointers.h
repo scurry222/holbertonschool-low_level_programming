5324dbcf75f193d1401ad9433b8bf280025550b4
scurry222 <scurry222@gmail.com>
2019-03-13 19:38:33 +0000

    moving library inside guard

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
-#include <stdlib.h>
-
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));

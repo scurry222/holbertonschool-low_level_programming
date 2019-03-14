571ecd7eab851e2af62991e79844e8c9675ccda8
scurry222 <scurry222@gmail.com>
2019-03-14 05:27:51 +0000

    get opcodes of args

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcodes of passed in args
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0 on success, exit if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}

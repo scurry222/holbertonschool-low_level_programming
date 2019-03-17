c73e4eeefe4e6d5fd9f1a57647a57451aedb4a74
scurry222 <scurry222@gmail.com>
2019-03-17 01:22:42 +0000

    Got better help on task 2

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(valist, char *);
 
 		if (!str)
-			printf("(nil)");
+			str = "(nil)";
 		if (!separator)
 			printf("%s", str);
 		else if (separator && i == 0)

e4416db6f60aaca39ac0c92a7574f61a69c33d8f
scurry222 <scurry222@gmail.com>
2019-03-17 15:44:52 +0000

    dive into assembly

---
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .text
+
+_start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, world!\n",
+  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  syscall           ; );
+
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "Hello, Holberton", 10
+  msglen: equ $ - msg

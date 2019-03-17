cde79431078043ace3ccee4537b3a8b2b9af42f0
scurry222 <scurry222@gmail.com>
2019-03-17 00:10:10 +0000

    fixing edge cases

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (n == 0)
 		printf("\n");
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < (n - 1); i++)
 	{
 		str = va_arg(valist, char *);
 

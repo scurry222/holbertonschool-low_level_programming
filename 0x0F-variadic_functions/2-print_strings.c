fc4278062f4277902ff132cde6cd499514ff7ab9
scurry222 <scurry222@gmail.com>
2019-03-17 01:18:50 +0000

    fixing if statement

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		if (!separator)
 			printf("%s", str);
-		if (separator && i == 0)
+		else if (separator && i == 0)
 			printf("%s", str);
 		else
 			printf("%s%s", separator, str);

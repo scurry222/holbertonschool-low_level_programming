1a0371b5644c6a4345964c9e2309b3cef65b8088
scurry222 <scurry222@gmail.com>
2019-03-26 17:34:31 +0000

    corrected boolean operator

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-	if (!new && !head)
+	if (!new || !head)
 		return (NULL);
 
 	new->n = n;

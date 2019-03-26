5ce03ab56e6a198b79cc1ecac0db2238d359cbb4
scurry222 <scurry222@gmail.com>
2019-03-26 21:20:48 +0000

    fixed formatting

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,14 +29,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	for (i = 0; temp && i < idx; i++)
-
+	{
 		if (i == idx - 1)
 		{
 			new->next = temp->next;
 			temp->next = new;
+			return (new);
 		}
 		else
 			temp = temp->next;
-
-	return (new);
+	}
+	return (NULL);
 }

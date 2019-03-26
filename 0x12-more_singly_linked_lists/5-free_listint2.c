8f58a713c53cda682bd9094cc2e786d4fb08a3b9
scurry222 <scurry222@gmail.com>
2019-03-26 17:28:40 +0000

    modifying temp variable instead of head

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,16 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *temp;
+
 	if (!head && !*head)
 		return;
 
 	while (*head)
 	{
+		temp = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
+		*head = temp;
 	}
 
 	*head = NULL;

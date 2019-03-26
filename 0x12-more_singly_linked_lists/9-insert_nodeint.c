0fddb82e449330af11265f9ea291dc19e51e6fe7
scurry222 <scurry222@gmail.com>
2019-03-26 21:18:57 +0000

    looping until end of idx

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,16 +23,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = temp;
-		temp = new;
+		new->next = *head;
+		*head = new;
 		return (new);
 	}
 
-	for (i = 0; temp && i < idx - 1; i++)
-		temp = temp->next;
+	for (i = 0; temp && i < idx; i++)
 
-	new->next = temp->next;
-	temp->next = new;
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+		}
+		else
+			temp = temp->next;
 
 	return (new);
 }

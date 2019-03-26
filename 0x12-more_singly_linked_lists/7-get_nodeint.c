8696712b5a780e39027f58d9c6557fbf0a908aca
scurry222 <scurry222@gmail.com>
2019-03-26 17:24:07 +0000

    modifying for efficency

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -14,15 +14,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 
 
-	for (i = 0; i < index; i++)
-	{
-		if (!temp)
-			return (NULL);
-
-		if (!temp->next)
-			break;
-
+	for (i = 0; temp && i < index; i++)
 		temp = temp->next;
-	}
+
+	if (!temp)
+		return (NULL);
+
 	return (temp);
 }

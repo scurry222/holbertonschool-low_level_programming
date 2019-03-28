4bd4d692b3aeb1394abafc494658475eeff4f2d9
scurry222 <scurry222@gmail.com>
2019-03-28 20:30:21 +0000

    increasing range of vars

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int diff, bits = 0;
+	unsigned long int diff, bits = 0;
 
 	for (diff = n ^ m; diff; diff = diff >> 1)
 		bits += diff & 1;

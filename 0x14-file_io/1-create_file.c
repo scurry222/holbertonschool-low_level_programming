fcfe34ca1c26a91bb5a604afffff1e4fb7f5b0ad
scurry222 <scurry222@gmail.com>
2019-04-02 20:43:50 +0000

    writing correct amount of bytes

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -21,11 +21,11 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i]; i++)
 		;
 
-	new = open(filename, O_RDWR | O_TRUNC | O_CREAT, 00600);
+	new = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (new == -1)
 		return (-1);
 
-	wcount = write(new, text_content, 1);
+	wcount = write(new, text_content, i);
 	if (wcount == -1)
 		return (-1);
 

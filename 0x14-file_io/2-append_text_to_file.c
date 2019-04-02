2e5a13fd5620dee1053f3d0bab25dc62cd82c631
scurry222 <scurry222@gmail.com>
2019-04-02 20:37:39 +0000

    checking  against -1 explicitly

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -19,14 +19,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		text_content = "";
 
 	fd = open(filename, O_RDWR | O_APPEND);
-	if (!fd)
+	if (fd == -1)
 		return (-1);
 
 	for (i = 0; text_content[i]; i++)
 		;
 
 	wcount = write(fd, text_content, i);
-	if (!wcount)
+	if (wcount == -1)
 		return (-1);
 
 	close(fd);

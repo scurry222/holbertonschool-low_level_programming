2cd53cdb09869aa0f04586bf2fbe006d14eb747f
scurry222 <scurry222@gmail.com>
2019-04-02 20:38:59 +0000

    checking against -1 explicitly

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -22,11 +22,11 @@ int create_file(const char *filename, char *text_content)
 		;
 
 	new = open(filename, O_RDWR | O_TRUNC | O_CREAT, 00600);
-	if (!new)
+	if (new == -1)
 		return (-1);
 
 	wcount = write(new, text_content, 1);
-	if (!wcount)
+	if (wcount == -1)
 		return (-1);
 
 	close(new);

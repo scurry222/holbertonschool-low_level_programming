dd254a6f6576257904483e8ff08e95740f81f98e
scurry222 <scurry222@gmail.com>
2019-04-02 22:38:18 +0000

    changing name of av to argv

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -3,11 +3,11 @@
 /**
 * main - make a new file and copy contents from another file
 * @argc: number of elements
-* @av: arg1: file 1 to copy from arg 2: file to copy into
+* @argv: arg1: file 1 to copy from arg 2: file to copy into
 *
 * Return: 0 on success, -1 on fail
 */
-int main(int argc, char **av)
+int main(int argc, char *argv[])
 {
 	int f_from, f_to, r, a, b;
 	char *buffer[1024];
@@ -17,25 +17,25 @@ int main(int argc, char **av)
 		dprintf(STDERR_FILENO, "Usage cp file_from file_to\n");
 		exit(97);
 	}
-	f_from = open(av[1], O_RDONLY);
+	f_from = open(argv[1], O_RDONLY);
 	if (f_from < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", argv[1]);
 		exit(98);
 	}
-	f_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	f_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((r = read(f_from, buffer, 1024)) > 0)
 	{
 		if (write(f_to, buffer, r) != r || f_to < 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can\'t write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can\'t write to %s\n", argv[2]);
 			close(f_from);
 			exit(99);
 		}
 	}
 	if (r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", argv[1]);
 		exit(98);
 	}
 	a = close(f_from);

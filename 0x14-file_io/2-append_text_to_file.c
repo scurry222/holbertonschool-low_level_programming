623a174d74e378a7d25cfbd8489438e33d9cfb7f
scurry222 <scurry222@gmail.com>
2019-04-02 18:32:11 +0000

    append text to end of file

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+* append_text_to_file - append text to end of file
+* @filename: name of file
+* @text_content: text to append
+*
+* Return: 1 on success, -1 on fail
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, wcount, fd;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (!fd)
+		return (-1);
+
+	for (i = 0; text_content[i]; i++)
+		;
+
+	wcount = write(fd, text_content, i);
+	if (!wcount)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}

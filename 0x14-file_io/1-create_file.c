00b8a6bacdf1a2e6a3ad33fb78a05a4d38d6a0d9
scurry222 <scurry222@gmail.com>
2019-04-02 18:20:20 +0000

    create file and add text

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+* create_file - create a file with a string
+* @filename: name of file
+* @text_content: string to write into file
+*
+* Return: 1 on success, -1 on fail
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, new, wcount;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (i = 0; text_content[i]; i++)
+		;
+
+	new = open(filename, O_RDWR | O_TRUNC | O_CREAT, 00600);
+	if (!new)
+		return (-1);
+
+	wcount = write(new, text_content, 1);
+	if (!wcount)
+		return (-1);
+
+	close(new);
+
+	return (1);
+}

9c4f1d98bb37107846bccde402c8cfd8aa52bc85
scurry222 <scurry222@gmail.com>
2019-04-02 22:26:34 +0000

    read and write

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,40 +1,52 @@
 #include "holberton.h"
 
 /**
+* main - make a new file and copy contents from another file
+* @argc: number of elements
+* @av: arg1: file 1 to copy from arg 2: file to copy into
 *
-*
-*
-*
+* Return: 0 on success, -1 on fail
 */
-
 int main(int argc, char **av)
 {
-	int cp, rcount, rcpcount;
-	char *buffer;
+	int f_from, f_to, r, a, b;
+	char *buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage cp file_from file_to\n");
 		exit(97);
 	}
-
-	buffer = open(av[1], O_RDONLY);
-	if (!rcount)
+	f_from = open(av[1], O_RDONLY);
+	if (f_from == -1)
 	{
-		free(buffer);
 		dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", av[1]);
 		exit(98);
 	}
-	rcpcount = open(av[2], O_O_RDWR | O_TRUNC);
-	if (!rcpcount)
+	f_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(f_from, buffer, 1024)) > 0)
 	{
-		close(rcount);
-		free(buffer);
-		dprintf(STDERR_FILENO, "Error: Can\'t write to %s\n", av[2]);
-		exit(99);
+		if (write(f_to, buffer, r) != r || f_to == -1)
+		{
+			close(f_from);
+			dprintf(STDERR_FILENO, "Error: Can\'t write to %s\n", av[2]);
+			exit(99);
+		}
 	}
-	(void) cp;
-	free(buffer);
-
+	if (r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	a = close(f_from);
+	b = close(f_to);
+	if (a < 0 || b < 0)
+	{
+		if (a < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from);
+		if (b < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to);
+		exit(100);
+	}
 	return (0);
 }

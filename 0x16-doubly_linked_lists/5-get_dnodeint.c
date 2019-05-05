b553422fd01d99f076bbff6c238bee66b19b6cb3
scurry222 <scurry222@gmail.com>
2019-05-05 18:43:16 +0000

    moving null check outside of loop

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -12,11 +12,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	for (i = 0; i < index; i++)
+	if (!head)
+		return (NULL);
+
+	for (i = 0; head && i < index; i++)
 	{
-		if (!head)
-			return (NULL);
-		else if (!head->next)
+		if (!head->next)
 			break;
 		head = head->next;
 	}

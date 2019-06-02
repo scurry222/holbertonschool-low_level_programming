b7be239511820980347c431ff33a4b83267cb52a
Scout Curry <scurry222@gmail.com>
2019-06-02 20:59:07 +0000

    if struct doesnt exist dont print

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -13,12 +13,15 @@ void hash_table_print(const hash_table_t *ht)
 	int flag = 0;
 	hash_node_t *node = NULL;
 
-	if (!ht || !ht->array || !ht->size)
+	if (!ht || !ht->array)
+	{
+		return;
+	}
+	if (!ht->size)
 	{
 		printf("{}\n");
 		return;
 	}
-
 	printf("{");
 	for (i = 0; i <= ht->size; i++)
 	{

dde8193961c15c359063ccb804b36d7571c3a130
Scout Curry <scurry222@gmail.com>
2019-06-19 21:48:18 +0000

    implementation of radix sort

---
+++ 0x1B-sorting_algorithms/105-radix_sort.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+
+/**
+ * count_sort_LSD - sort by LSD
+ * @array: array to sort
+ * @size: size of the array
+ * @lsd: least significant digit
+ */
+void LSD_sort(int *array, size_t size, size_t lsd)
+{
+	int bucket[10] = {0}, *dump, l, k;
+	size_t i, j;
+
+	dump = malloc(sizeof(int) * size);
+
+	for (i = 0; i < size; i++)
+		bucket[(array[i] / lsd) % 10]++;
+
+	for (l = 1; l < 10; l++)
+		bucket[l] += bucket[l - 1];
+
+	for (k = size - 1; k >= 0; k--)
+	{
+		dump[bucket[(array[k] / lsd) % 10] - 1] = array[k];
+		bucket[(array[k] / lsd) % 10]--;
+	}
+
+	for (j = 0; j < size; j++)
+		array[j] = dump[j];
+
+	free(dump);
+}
+
+/**
+ * radix_sort - implementation of radix sort
+ * @array: array of ints to sort
+ * @size: size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max = 0;
+	size_t i, lsd;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	for (lsd = 1; max / lsd > 0; lsd *= 10)
+	{
+		LSD_sort(array, size, lsd);
+		print_array(array, size);
+	}
+}
+
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -18,37 +18,48 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* printing and swapping universal functions */
 void print_list(const listint_t *list);
-
+void swap_int(int *a, int *b);
 void print_array(const int *array, size_t size);
 
+/* 0 - bubble sort */
 void bubble_sort(int *array, size_t size);
 
-void swap_int(int *a, int *b);
-
+/* 1 - insertion sort */
 void insertion_sort_list(listint_t **list);
 
+/* 2 - selection sort */
 void selection_sort(int *array, size_t size);
 
+/* 3 - quick sort */
 void quick_sort(int *array, size_t size);
 void sort_recurse(int *array, ssize_t lo, ssize_t hi, size_t size);
 ssize_t partition(int *array, ssize_t lo, ssize_t hi, size_t size);
 
+/* 100 - shell sort */
 void shell_sort(int *array, size_t size);
 size_t list_len(listint_t *list);
 void switch_nodes(listint_t *a, listint_t *b);
 
+/* 101 - cocktail sort */
 void cocktail_sort_list(listint_t **list);
 listint_t *trav_rev(listint_t *head, listint_t *tail, listint_t *list);
 
+/* 102 - counting sort */
 void counting_sort(int *array, size_t size);
 
+/* 103 - merge sort */
 void merge_sort(int *array, size_t size);
 void merge_recurse(int *sub, int *array, size_t left, size_t right);
 void merge_with_subarray(int *sub, int *array, size_t left,
 			size_t middle, size_t right);
-
+/* 104 - heap sort */
 void heap_sort(int *array, size_t size);
 void heapify(int *array, size_t end, size_t start, size_t size);
+
+/* 105 - radix sort */
+void radix_sort(int *array, size_t size);
+void LSD_sort(int *array, size_t size, size_t lsd);
 
 #endif

9b111920f2b5a5ef585c7d92ddf21c143c28b88e
Scout Curry <scurry222@gmail.com>
2019-07-11 21:22:57 +0000

    mandatory tasks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """ Return the perimeter of the island from the grid
+
+	Args:
+            grid(2D array, ints): 0 repr water, 1 repr land
+                                not connected diagonally
+                              grid < 100
+    """
+    p = 0
+    h = len(grid)
+    w = len(grid[0]) 
+
+    for x in range(h):
+        for y in range(w):
+            if grid[x][y] == 1:
+                if x - 1 < 0 or grid[x - 1][y] == 0:
+                    p += 1
+                if x + 1 >= w or grid[x + 1][y] == 0:
+                    p += 1
+                if y - 1 < 0 or grid[x][y - 1] == 0:
+                    p += 1
+                if y + 1 >= h or grid[x][y + 1] == 0:
+                    p += 1
+    return p

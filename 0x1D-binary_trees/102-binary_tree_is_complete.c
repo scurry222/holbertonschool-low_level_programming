4b1c98f1a34f0042dd7b744c7d96acfe1f48d168
Scout Curry <scurry222@gmail.com>
2019-07-25 21:00:44 +0000

    betty compliant

--- 0x1D-binary_trees/102-binary_tree_is_complete.c
+++ 0x1D-binary_trees/102-binary_tree_is_complete.c
@@ -9,15 +9,15 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        size_t r_size, l_size;
+	size_t r_size, l_size;
 
-        if (!tree)
-                return (0);
+	if (!tree)
+		return (0);
 
-        r_size = binary_tree_size(tree->right);
-        l_size = binary_tree_size(tree->left);
+	r_size = binary_tree_size(tree->right);
+	l_size = binary_tree_size(tree->left);
 
-        return (r_size + l_size + 1);
+	return (r_size + l_size + 1);
 }
 
 /**

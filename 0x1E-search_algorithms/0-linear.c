3df429e6f6ddc952601266ac9b5dd84ee5e2a3f2
Scout Curry <scurry222@gmail.com>
2019-08-28 22:18:34 +0000

    print and conditionals

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,8 +15,12 @@ int linear_search(int *array, size_t size, int value)
 	int i;
 	int s = (int)size;
 
+	if (!array || size == 0)
+		return (-1);
+
 	for (i = 0; i < s; i++)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}

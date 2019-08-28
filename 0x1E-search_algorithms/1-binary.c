b021951401e6ca97c271b6c0e0937962b0e52f39
Scout Curry <scurry222@gmail.com>
2019-08-28 22:20:50 +0000

    adding space in printing

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (bot <= top)
 	{
-		printf("Searching in array:");
+		printf("Searching in array: ");
 		for (i = bot; i < top; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
